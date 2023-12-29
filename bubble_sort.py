86bc00e4a48ebe2898cde681950e574ff5b98f41
muggerseo <mugger.seo@gmail.com>
2023-12-29 23:29:51 +0200

    other commits

--- bubble_sort.py
+++ bubble_sort.py
@@ -34,15 +34,37 @@ def sort_decrease():
 def set_window_style():
     s = ttk.Style()
     s.configure('TFrame', background='lightgrey')
-    s.configure('TButton', background='orange', foreground='black', font=('Times new Roman', 10, 'bold'), padding=(10,5))
-    s.configure('TLabel', foreground='black', font=('Arial', 10, 'bold'))
+    s.configure('TLabel', background='lightgrey', foreground='black', font=('Arial', 10, 'bold'))
+    s.configure('TButton', background='orange', foreground='black',
+                font=('Times new Roman', 10, 'bold'), padding=(10,5))
+
+def center_window(window, width, height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    x_coordinates = (screen_width - width) // 2
+    y_coordinates = (screen_height - height) // 2
+
+    window.geometry(f"{width}x{height}+{x_coordinates}+{y_coordinates}")
+   
+def reset_fields():
+    answer = messagebox.askquestion('Reset input field?')
+    if answer == 'yes':
+        input_1.delete(0, tk.END)
+        input_1.focus_set()
+        result_label.config(text="")
+    elif answer == 'NO':
+        messagebox.showinfo("Canselled", "Reset canceled")
 
 # seq = 7 33 73 642 4322 6677 0 1 2 
 
 window = tk.Tk()
 window.title("Sort prog")
-window.geometry('250x300')
-window.resizable(True,True)
+#window.geometry('250x300')
+window_width = 250
+window_height = 300
+#window.resizable(True,True)
+center_window(window, window_width, window_height)
 
 set_window_style()
 
@@ -52,7 +74,7 @@ input_frame.grid(pady=10)
 label_1 = ttk.Label(input_frame, text="Enter numbers, space separeted: ", style='TLabel')
 label_1.grid(row=0, columnspan=2, pady=10, padx=20)
 input_1 = tk.Entry(input_frame, bg='white')
-input_1.grid(row=1, columnspan=2)
+input_1.grid(row=1, columnspan=2, sticky='N')
 input_1.focus_set()
 input_1.bind('<Return>', lambda event: get_input())
 
@@ -60,15 +82,22 @@ input_button = ttk.Button(input_frame, text='Submit', command=get_input, style='
 input_button.grid(row=2, columnspan=2, pady=10)
 
 increase_button = ttk.Button(input_frame, text='Increase', command=sort_increase)
-increase_button.grid(row=3, column=0, pady=10)
+increase_button.grid(row=3, column=0)
 increase_button.bind('<Return>', lambda event: sort_increase())
 
 decrese_button = ttk.Button(input_frame, text='Decrease', command=sort_decrease)
-decrese_button.grid(row=3, column=1, pady=10)
+decrese_button.grid(row=3, column=1)
 decrese_button.bind('<Return>', lambda event: sort_decrease())
 
-result_label = ttk.Label(input_frame, text="")
-result_label.grid(row=5, columnspan=2, pady=20)
+result_frame = ttk.Frame(window, style='TFrame')
+result_frame.grid(row=5, columnspan=2, pady=10)
+
+result_label = ttk.Label(result_frame, style='TLabel', text="")
+result_label.grid(row=6, columnspan=2, pady=20)
+
+reset_button = ttk.Button(input_frame, text='Reset', command=reset_fields)
+reset_button.grid(row=7, columnspan=2, pady=20)
+reset_button.bind('<Return>', lambda event=NONE:reset_fields())
 
 
 if __name__ == '__main__':
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+result_label = ttk.Label(input_frame, text="")
